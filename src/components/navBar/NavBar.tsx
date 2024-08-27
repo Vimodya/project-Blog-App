569cccf3116279dea4fe732bdeddeb48f4cdb6a1
Liyanage C.V.K <130178817+Vimodya@users.noreply.github.com>
2024-08-27 13:55:35 +0100

    fix: fix bug
    bug fix about login

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -1,22 +1,16 @@
-// src/components/navBar/NavBar.tsx
 "use client";
-
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
-import { useAuth } from "./../../app/context/AuthProvider";
 
 export default function NavBar() {
-  const [open, setOpen] = useState(false);
-  const { isLoggedIn } = useAuth();
-
   return (
     <div>
       <div className="flex flex-row justify-between mx-8">
         <div className="flex items-center mt-4">
           <Image src="/w.png" alt="Logo" width={50} height={50} />
         </div>
-        <div className="text-[#192841] space-x-4 hidden md:flex lg:flex mt-8">
+        <div className="text-[#192841] space-x-4 flex mt-8">
           <Link href="/home" className="hover:text-yellow-500">
             Home
           </Link>
@@ -26,47 +20,12 @@ export default function NavBar() {
           <Link href="/blogs" className="hover:text-yellow-500">
             Blogs
           </Link>
-          {!isLoggedIn && (
-            <Link href="/login" className="hover:text-yellow-500">
-              LogIn
-            </Link>
-          )}
+
+          <Link href="/login" className="hover:text-yellow-500">
+            LogIn
+          </Link>
         </div>
       </div>
-      <div className="">
-        <button
-          onClick={() => setOpen(true)}
-          className="flex md:hidden lg:hidden justify-end text-[#192841] top-0 right-0 bg-opacity-50 hover:text-yellow-500"
-        >
-          Menu
-        </button>
-      </div>
-      {open && (
-        <div className="bg-white">
-          <div className="absolute w-full h-full bg-white bg-opacity-50">
-            <button onClick={() => setOpen(false)}>Close</button>
-            <div className="flex flex-col justify-end text-[#192841] space-y-4">
-              <Link href="/" className="hover:text-yellow-500">
-                Home
-              </Link>
-              <Link href="/about" className="hover:text-yellow-500">
-                About
-              </Link>
-              <Link href="/contacts" className="hover:text-yellow-500">
-                Contact
-              </Link>
-              <Link href="/blogs" className="hover:text-yellow-500">
-                Blogs
-              </Link>
-              {!isLoggedIn && (
-                <Link href="/login" className="hover:text-yellow-500">
-                  LogIn
-                </Link>
-              )}
-            </div>
-          </div>
-        </div>
-      )}
     </div>
   );
 }
